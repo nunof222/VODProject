1ed7cb00aac954bef9b481bd9a0c66d42965bc0d
nunoff222 <19679544+nunof222@users.noreply.github.com>
2022-11-13 05:31:23 +0000

    comment out unfinished path / fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { PageNotFoundComponent } from './Components/page-not-found/page-not-foun
 
 const routes: Routes = [
   {path: 'home', component: AppComponent},
-  { path: '', pathMatch: 'full', redirectTo: 'AppComponent' },
-  {
-    path: 'path', 
-    component: Component,
-    children: [
-      { path: 'childpath', component: ChildComponent }  //usar childPaths no admin module
-    ]
-  },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  // {
+  //   path: 'path', 
+  //   component: Component,
+  //   children: [
+  //     { path: 'childpath', component: ChildComponent }  //usar childPaths no admin module
+  //             ]
+  // },
   { path: '**', component: PageNotFoundComponent },
 ];
 
