b56adb560ec545707c44c1543bfca243baa22a4f
nunoff222 <19679544+nunof222@users.noreply.github.com>
2022-11-17 01:51:03 +0000

    add auth service

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { User } from 'src/app/Domain/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private userSubject: BehaviorSubject<User | null>;
+  public user: Observable<User | null>;
+
+  constructor(private http: HttpClient) {
+    this.userSubject = new BehaviorSubject<User | null>(JSON.parse(localStorage.getItem('user')!)
+    );
+    this.user= this.userSubject.asObservable();
+   }
+
+   login(email:string, password:string) {
+    return this.http.post<User>('/api/user/signin', {email, password})
+      .pipe(map(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+        return user;
+      }
+        ));
+   }
+   
+   logout() {localStorage.removeItem('user');}
+
+   getToken():string {
+    return this.userSubject.value!.token;
+   }
+}
