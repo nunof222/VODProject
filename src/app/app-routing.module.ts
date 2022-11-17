fd9c7e7712eb788694e673e1bd6ab5828961b8ea
nunoff222 <19679544+nunof222@users.noreply.github.com>
2022-11-17 01:40:13 +0000

    change default route to home comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'home', component: AppComponent},
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  {path: 'home', component: HomeComponent},
+   { path: '', pathMatch: 'full', redirectTo: '/home' },
   // {
   //   path: 'path', 
   //   component: Component,
