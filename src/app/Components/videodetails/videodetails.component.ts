b0ac1569d5d2c10fcf18052c328a9256aecb3883
nunoff222 <19679544+nunof222@users.noreply.github.com>
2022-11-19 03:05:04 +0000

    implement yt player on video details component

--- src/app/Components/videodetails/videodetails.component.ts
+++ src/app/Components/videodetails/videodetails.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { YouTubePlayer } from '@angular/youtube-player';
+import { Video } from 'src/app/Domain/video';
+
 
 @Component({
   selector: 'app-videodetails',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./videodetails.component.css']
 })
 export class VideoDetailsComponent implements OnInit {
+  videoId:string = 'ifTF3ags0XI';
+  video = [];
+
 
   constructor() { }
 
   ngOnInit(): void {
+    if (!apiLoaded) {
+      // This code loads the IFrame Player API code asynchronously, according to the instructions at
+      // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      apiLoaded = true;
+    }
   }
 
 }
+
+let apiLoaded = false;
